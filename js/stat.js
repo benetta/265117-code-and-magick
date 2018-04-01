32c986e1065263fe8792db9212c46ae6778a7f88
benetta <ksenyatokareva@gmail.com>
2018-04-01 22:16:02 +0300

    правки по stylelint

--- js/stat.js
+++ js/stat.js
@@ -1,31 +1,34 @@
+'use strict';
+
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getHeight = function(times, num) {
+var getHeight = function (times, num) {
   var maxTime = times[0];
-  for(var i = 0; i < times.length; i++) {
-    if( maxTime < Math.round(times[i]) ) { maxTime = Math.round(times[i]) };
-  };
+  for (var i = 0; i < times.length; i++) {
+    if (maxTime < Math.round(times[i])) {
+      maxTime = Math.round(times[i]);
+    }
+  }
 
   var percent = Math.round((times[num] * 100) / maxTime);
   return 150 * percent / 100;
 };
 
-var renderColumn = function(ctx, x, y, names, times) {
+var renderColumn = function (ctx, x, y, names, times) {
   var COLUMN_WIDTH = 40;
-  var COLUMN_HEIGHT = 150;
   var TOP_TEXT_POSITION = 85;
   var BOTTOM_TEXT_POSITION = 265;
   var xInitial = 120;
   var height = 0;
   var yPosition = 0;
 
-  names.forEach( function(name, i) {
+  names.forEach(function (name, i) {
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
@@ -44,7 +47,7 @@ var renderColumn = function(ctx, x, y, names, times) {
   });
 };
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#fff');
 
